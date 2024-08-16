4339f9c2d4f99e19173514a65fa49239f7eddd71
subhasheemani <124122309+subhasheemani@users.noreply.github.com>
2024-08-16 11:58:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeSection from './Components/HomeSection';
+
+function App() {
+  return (
+    <div className="overflow-x-hidden App">
+      <HomeSection></HomeSection>
+    </div>
+  );
+}
+
+export default App;
